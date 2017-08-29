dc7bdd5c90a7632fabae7bf6a7cb6b80ff0f031c
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2017-08-29 21:50:34 +0200

    feat: stash the dirty laundry during deploys

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,11 @@
 
 set -ex
 
+# Stash changes
+git stash -u
+
 bundle exec middleman build --clean
 rsync -e "ssh -p 222" -avz build/ zadevchat.io@zadevchat.io:/var/www/zadevchat.io/
+
+# Get the changes back
+git stash pop
